c4cb68265b51a8a95c6733c383ed11b45b23d955
sakshii2216 <shindesakshi8080@gmail.com>
2024-10-17 13:31:01 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class Array {
+
+	public static void main(String[] args) {
+		String animals[]= {"Cat","Dog","Lion","Tiger","Monkey","Cow","Bull","Elephant","Sheep","Rabit","Rat"};
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(animals[i]);
+		}
+	}
+}
