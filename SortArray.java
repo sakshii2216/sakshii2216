4825822a4593201575894f7310d9401025bb8d05
sakshii2216 <shindesakshi8080@gmail.com>
2024-10-24 14:27:59 +0530

    Add files via upload

---
+++ SortArray.java
@@ -0,0 +1,21 @@
+package Array;
+import java.util.*;
+
+public class SortArray {
+
+	public static void main(String[] args) {// Java Program to Sort the Elements in Descending Order
+		    {
+		        Integer a[] = { 1, 2, 3, 4, 5 };
+
+		        // Sorting the array in descending order
+		        Arrays.sort(a, Collections.reverseOrder());
+
+		        System.out.println(Arrays.toString(a));
+		    }
+		}
+
+		// TODO Auto-generated method stub
+
+	}
+
+
