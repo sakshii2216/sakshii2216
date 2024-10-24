f9efb4e7e578c56e5ee46ef7aacfe617c4459357
sakshii2216 <shindesakshi8080@gmail.com>
2024-10-24 14:27:10 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,23 @@
+package Array;
+
+		import java.util.Arrays;
+		import java.util.Collections;
+
+
+		public class ReverseArray {
+
+			public static void main(String[] args) {
+				        // Create an array
+				        Integer[] arr = {51, 52, 43, 24, 95};
+				        
+				        // Reverse the array
+				        Collections.reverse(Arrays.asList(arr));
+
+				        // Print the reversed array
+				        System.out.println("Reversed Array: " + Arrays.toString(arr));
+				    }
+				
+		// TODO Auto-generated method stub
+
+	}
+
